b8a1e063b93934df89ecf612ab6f3d2038f6e416
Alexander Brown <alex@alexanderdbrown.com>
2012-11-14 10:45:43 +0000

    Issue #5: Started propagation of the users view.
    Unfortunately the pagination gem affects the :all request on the ActiveResource class and only returns the first 6 results. I can see two ways around this:
    
    * Go through all the pages and load everyone.
    * Include pagination in the client.
    
    Either one will be a bit annoying to implement, but sort of refractoring the server code to work with and without pagination (and if it ain't broke, don't fix it) then any other solution will just be too complex.

--- src/client/src/gui/users.rb
+++ src/client/src/gui/users.rb
@@ -15,5 +15,38 @@ class UserView < FXVerticalFrame
   def setup
     FXLabel.new(self, "Users").font=@titleFont
     #TODO
+
+    @header1 = FXHeader.new(self, 
+      :opts => HEADER_BUTTON|HEADER_RESIZE|FRAME_RAISED|FRAME_THICK|LAYOUT_FILL_X)
+    @header1.appendItem("Surname", nil, 150)
+    @header1.appendItem("First name", nil, 150)
+    @header1.appendItem("Email", nil, 150)
+    @header1.appendItem("Grad Year", nil, 150)
+
+    panes = FXHorizontalFrame.new(self,
+      FRAME_SUNKEN|FRAME_THICK|LAYOUT_SIDE_TOP|LAYOUT_FILL_X|LAYOUT_FILL_Y,
+      :padLeft => 0, :padRight => 0, :padTop => 0, :padBottom => 0,
+      :hSpacing => 0, :vSpacing => 0)
+
+    @lists = []
+    @lists.push(FXList.new(panes, :opts => LAYOUT_FILL_Y|LAYOUT_FIX_WIDTH|LIST_SINGLESELECT,   :width => 150))
+    @lists.push(FXList.new(panes, :opts => LAYOUT_FILL_Y|LAYOUT_FIX_WIDTH|LIST_SINGLESELECT,   :width => 150))
+    @lists.push(FXList.new(panes, :opts => LAYOUT_FILL_Y|LAYOUT_FIX_WIDTH|LIST_SINGLESELECT,   :width => 150))
+    @lists.push(FXList.new(panes, :opts => LAYOUT_FILL_Y|LAYOUT_FIX_WIDTH|LIST_SINGLESELECT,   :width => 150))
+  end
+
+  def refresh()
+    self.propagate(@client.users(:all))
+  end
+
+  def propagate(data)
+    for list in @lists do
+      list.clearItems()
+    end
+
+    for entry in data do
+	      @lists[0].appendItem(entry.surname)
+      @lists[1].appendItem(entry.firstname)
+    end
   end
 end
