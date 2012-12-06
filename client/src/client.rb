795a3356e06dcd335468295211e9764d93e594f8
Alex Brown <alex@alexanderdbrown.com>
2012-12-06 15:29:38 +0000

    Updated commenting.

--- client/src/client.rb
+++ client/src/client.rb
@@ -5,30 +5,35 @@ require 'parseconfig'
 require 'active_resource'
 require 'logger'
 
+# Class to perform interaction with the Rails server.
 class Client
+  # Config file to use.
   CONFIG_FILE = './csa.conf'
+  
+  # Logger object which will log to the file defined in the logfile.
   LOGGER = Logger.new(ParseConfig.new(CONFIG_FILE)['logfile'])
 
-  def initialize(user=nil, password=nil)
+  # Initialise using some of the config file.
+  def initialize()
+    # Load the site from the config.
     parser = ParseConfig.new(CONFIG_FILE)
     @site = parser['site']
-    @user = user
-    @password = password
+    
+    # Prepare the object with nil values.
+    @user = nil
+    @password = nil
     @error = nil
     @logged_in = false
     @is_admin = false
 
     User.site = @site
-    User.user = @user
-    User.password = @password
     User.logger = LOGGER
 
     Broadcast.site = @site
-    Broadcast.user = @user
-    Broadcast.password = @password
     Broadcast.logger = LOGGER
   end
 
+  # Attempt to login to the system.
   def login(user, password)
     @user = user
     @password = password
@@ -39,11 +44,13 @@ class Client
     Broadcast.user = @user
     Broadcast.password = @password
 
+    # Set cached variables to save a lot of REST requests.
     @logged_in = currentUserExists?
     @is_admin = checkIsAdmin?
     return @logged_in
   end
 
+  # Perform a logout.
   def logout
     @user = nil
     @password = nil
@@ -57,28 +64,39 @@ class Client
     Broadcast.password = nil
   end
 
+  # Get the current error message.
   def getError
     return @error
   end
 
+  # true if the user is definitely logged into the CSA server.
   def loggedIn?
     return @logged_in
   end
 
+  # true if the user is an admin user.
   def isAdmin?
     return @is_admin
   end
 
+  # Check if the current user is an admin user.
   def checkIsAdmin?
+    # Get the current user.
     user = User.get(:current)
+    
+    # An admin user is defined by their username being 'admin'.
     return user['login'] == 'admin'
   rescue ActiveResource::UnauthorizedAccess => e
     @error = e.message
     return false
   end
 
+  # Check the user that we're trying to login with exists.
   def currentUserExists?
+    # Get the current user.
     user =  User.get(:current)
+    
+    # Check it exists.
     return User.exists?(user["id"])
   rescue ActiveResource::UnauthorizedAccess
     @error = "User unauthorized."
@@ -88,6 +106,7 @@ class Client
     return false
   end
 
+  # Get the current user.
   def currentUser
     user = User.get(:current)
     return User.find(user["id"])
@@ -99,6 +118,7 @@ class Client
     return nil
   end
 
+  # Load a user or a set of users. See ActiveResource for more information.
   def users(*parameters)
     return User.find(*parameters)
   rescue ActiveResource::UnauthorizedAccess
@@ -108,6 +128,7 @@ class Client
     return nil
   end
 
+  # Load a broadcast or set of broadcasts. See ActiveResource for more information.
   def broadcasts(*parameters)
     return Broadcast.find(*parameters)
   rescue ActiveResource::UnauthorizedAccess
