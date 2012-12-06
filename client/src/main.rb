6dd0f5509e54491b43348668cb947bbbec4efc5a
Alex Brown <alex@alexanderdbrown.com>
2012-12-06 15:30:14 +0000

    Updated commenting.

--- client/src/main.rb
+++ client/src/main.rb
@@ -1,6 +1,11 @@
 require_relative 'client.rb'
 require_relative 'gui/main.rb'
 
+# Create a new client
 client = Client.new()
+
+# Create a new GUI and run it.
 gui = GUI.new(client)
 gui.run()
+
+# That's all we need!
