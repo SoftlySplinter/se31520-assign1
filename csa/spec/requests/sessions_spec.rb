2792c3bfbb4395cf988e7061569076931b6d748f
Alex Brown <alex@alexanderdbrown.com>
2012-12-06 11:01:34 +0000

    Issue #12: Added commenting to the RSpec test.

--- csa/spec/requests/sessions_spec.rb
+++ csa/spec/requests/sessions_spec.rb
@@ -1,48 +1,65 @@
 require 'spec_helper'
 
+# Sessions testing.
 describe "Sessions" do
   describe "Login to the CSA Server" do
+  
     it "Has a button to the login form" do
+      # Go to the home page
       get '/home'
       response.status.should be(200)
 
+      # Find the form with the button to go to the login page.
       assert_select 'form', action: '/session/new' do
         assert_select 'input#login'
       end
     end
 
     it "Logs in using the login form." do
+      # Create a user detail.
       user_detail = UserDetail.create!(login: 'test', password: 'test')
+      
+      # Go to the login page.
       get '/session/new'
       response.status.should be(200)
 
+      # Find the login form.
       assert_select 'form#auth', action: '/session' do
         assert_select 'input[name=?]', 'login'
         assert_select 'input[name=?]', 'password'
         assert_select 'input[name=?]', 'commit'
       end
 
+      # Post to the action the form performs.
       post '/session', login: user_detail.login, password: user_detail.password
+      
+      # As the information is correct we expect to be redirected to the home page.
       expect(response).to redirect_to('/home')
 
+      # To check everything is correct go to the home page and see if the application has registred the login.
       get '/home'
-
       assert_select '.login-form p', text: "Welcome #{user_detail.login}"
     end
 
     it "Fails to login with incorrect details." do
+      # Create the user.
       user_detail = UserDetail.create!(login: 'test', password: 'test')
+      
+      # Go to the login page.
       get '/session/new'
       response.status.should be(200)
 
+      # Find the login form.
       assert_select 'form#auth', action: '/session' do
         assert_select 'input[name=?]', 'login'
         assert_select 'input[name=?]', 'password'
         assert_select 'input[name=?]', 'commit'
       end
 
+      # Post to the action of the login form with incorrect details.
       post '/session', login: user_detail.login, password: 'incorrect'
 
+      # Check there's a flash message for that says the user wasn't logged in.
       # Annoyingly we have to change the ' to a HTML encoded form here.
       assert_select '#flash', text: "#{I18n.t('sessions.login-failure')} #{user_detail.login}".sub('\'', '&#x27;')
     end
