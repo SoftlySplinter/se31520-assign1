4bb7b6bdffe6184bb06d58481fc4709d66b5f257
Alexander Brown <alex@alexanderdbrown.com>
2012-12-09 10:43:36 +0000

    Added some commenting.

--- client/src/gui/main.rb
+++ client/src/gui/main.rb
@@ -16,6 +16,7 @@ class ShutterButton < FXButton
   end
 end
 
+# The main GUI Class.
 class GUI
   def initialize(client)
     @client = client
@@ -27,42 +28,53 @@ class GUI
     self.setup
   end
 
+  # Set up the GUI.
   def setup()
+    # Window splitter.
     splitter = FXSplitter.new(@window, (LAYOUT_SIDE_TOP|LAYOUT_FILL_X|
       LAYOUT_FILL_Y|SPLITTER_TRACKING))
 
+    # Shutter down the LHS
     shutter = FXShutter.new(splitter,
       :opts => FRAME_SUNKEN|LAYOUT_FILL_X|LAYOUT_FILL_Y,
       :padding => 0, :hSpacing => 0, :vSpacing => 0, :width => 100)
 
+    # Window switcher on the RHS
     switcher = FXSwitcher.new(splitter,
       FRAME_SUNKEN|LAYOUT_FILL_X|LAYOUT_FILL_Y, :padding => 0)
 
+    # Setup up both of the above.
     self.setupSwitcher(switcher)
     self.setupShutter(shutter, switcher)
   end
 
+  # Set up the page switcher.
   def setupSwitcher(switcher)
+    # Create the blank sections.
     self.createHomeSection(switcher)
     self.createJobsSection(switcher)
+
+    # Create objects for the proper views.
     @profileView = ProfileView.new(@app, @client, switcher)
     @userView = UserView.new(@app, @client, switcher)
     @broadcastView = BroadcastView.new(@app, @client, switcher)
   end
 
+  # Builds the home section, just displays text.
   def createHomeSection(switcher)
     home = FXVerticalFrame.new(switcher)
     FXLabel.new(home, "Welcome to CS-Alumni News").font=@titleFont
     FXLabel.new(home, "The is where the home page text will go.")
   end
 
+  # Builds the jobs section (which does nothing as of yet).
   def createJobsSection(switcher)
     jobs = FXVerticalFrame.new(switcher)
     FXLabel.new(jobs, "Jobs").font=@titleFont
     FXLabel.new(jobs, "Placeholder for the jobs screen.")
   end
 
-
+  # Set up the menu shutter.
   def setupShutter(shutter, switcher)
     shutterItem = FXShutterItem.new(shutter, "Menu", nil, LAYOUT_FILL_Y)
     ShutterButton.new(shutterItem.content, "Home").connect(SEL_COMMAND) { switcher.current = 0 }
@@ -94,7 +106,8 @@ class GUI
     self.enableForLoginAndAdmin(userBut)
     self.enableForLoginAndAdmin(broadBut)
   end
-  
+
+  # Set up triggers for enabling buttons on login.  
   def enableForLogin(button)
     button.connect(SEL_UPDATE) do |sender, sel, ptr|
        message = @client.loggedIn? ? FXWindow::ID_ENABLE : FXWindow::ID_DISABLE
@@ -103,6 +116,7 @@ class GUI
   end
 
 
+  # Set up triggers for enabling buttons on login of an admin user.
   def enableForLoginAndAdmin(button)
     button.connect(SEL_UPDATE) do |sender, sel, ptr|
        message = @client.loggedIn? && @client.isAdmin? ? FXWindow::ID_ENABLE : FXWindow::ID_DISABLE
@@ -110,6 +124,7 @@ class GUI
     end
   end
 
+  # Run the GUI.
   def run()
     @app.create
     @window.show
